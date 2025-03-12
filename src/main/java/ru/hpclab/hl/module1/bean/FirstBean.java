708e9c3a2bb566b36d87894c1520dd04c63959ef
Савченко Вероника Алексеевна <veronika.savchenko@rt.ru>
2025-03-12 09:16:30 +0300

    Start

---
+++ src/main/java/ru/hpclab/hl/module1/bean/FirstBean.java
@@ -0,0 +1,12 @@
+package ru.hpclab.hl.module1.bean;
+
+public class FirstBean {
+    private final String greeting = "Hello world!";
+
+    public FirstBean() {
+    }
+
+    public String getGreeting() {
+        return greeting;
+    }
+}
---
+++ src/main/java/ru/hpclab/hl/module1/bean/SecondBean.java
@@ -0,0 +1,12 @@
+package ru.hpclab.hl.module1.bean;
+
+public class SecondBean extends FirstBean {
+    private final String greeting = "World was investigated";
+
+    public SecondBean() {
+    }
+
+    public String getGreeting() {
+        return greeting;
+    }
+}
